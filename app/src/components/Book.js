93d6e095562aff6763b80cde78d4da5692824d92
Yunusemre Şentürk <se.yunusemre@gmail.com>
2023-12-15 22:00:43 +0300

    Add missing pages 5,6 and update some cosmetics
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/components/Book.js
+++ app/src/components/Book.js
@@ -9,7 +9,7 @@ import appendicesContent from '../assets/appendices.json';
 
 
 const Book = () => {
-    const [currentPage, setCurrentPage] = useState(parseInt(localStorage.getItem("qurantft-pn")) ? parseInt(localStorage.getItem("qurantft-pn")) : 13);
+    const [currentPage, setCurrentPage] = useState(parseInt(localStorage.getItem("qurantft-pn")) ? parseInt(localStorage.getItem("qurantft-pn")) : 5);
     const [pageHistory, setPageHistory] = useState([]);
     const [isModalOpen, setModalOpen] = useState(false);
     const [selectedSura, setSelectedSura] = useState(null);
@@ -44,7 +44,16 @@ const Book = () => {
 
 
     const nextPage = () => {
-        updatePage(parseInt(currentPage) + 1);
+
+        let newPage = parseInt(currentPage) >= 508 ? parseInt(currentPage) : parseInt(currentPage) + 1;
+
+        // Skip specified pages
+        const skipPages = [7, 8, 9, 10, 11, 12];
+        while (skipPages.includes(newPage)) {
+            newPage++;
+        }
+
+        updatePage(newPage);
     };
 
     const prevPage = () => {
@@ -54,13 +63,17 @@ const Book = () => {
             setPageHistory(prevHistory => prevHistory.slice(0, -1));
             setCurrentPage(lastPage);
         } else {
-            // If history is empty and the current page is not 13, decrement the page
-            if (currentPage !== 13) {
-                setCurrentPage(prevPage => prevPage > 1 ? prevPage - 1 : 1);
+            // Skip specified pages when decrementing
+            const skipPages = [7, 8, 9, 10, 11, 12];
+            let newPage = parseInt(currentPage) > 5 ? parseInt(currentPage) - 1 : parseInt(currentPage);
+
+            while (skipPages.includes(newPage)) {
+                newPage--;
             }
+
+            setCurrentPage(newPage);
         }
     };
-
 
     const createReferenceMap = () => {
         const referenceMap = {};
@@ -125,7 +138,7 @@ const Book = () => {
                 return (
                     <span
                         key={index}
-                        className="cursor-pointer text-sky-300"
+                        className="cursor-pointer text-sky-400"
                         onClick={() => handleClickReference(part)}>
 
                         {part}
@@ -138,14 +151,6 @@ const Book = () => {
 
     const renderBookContent = () => {
 
-        if (parseInt(currentPage) >= 395 && parseInt(currentPage) <= 396) {
-            return (
-                <div className="w-screen h-screen flex items-center justify-center text-neutral-300 text-3xl font-bold ">
-                    Appendices
-                </div>
-            );
-        }
-
         // Render Pages component when current page is 23 or more
         if (parseInt(currentPage) >= 23 && parseInt(currentPage) <= 394) {
             return <Pages selectedPage={currentPage} selectedSura={selectedSura} selectedVerse={selectedVerse} handleClickReference={handleClickReference} />;
@@ -154,7 +159,11 @@ const Book = () => {
         if (parseInt(currentPage) === 22) {
             return (
                 <div className="w-screen h-screen flex items-center justify-center text-neutral-300">
-                    Sura List Loading...
+                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                     Sura List Loading...
                 </div>
             );
         }
@@ -163,8 +172,14 @@ const Book = () => {
         const currentPageData = bookContent ? bookContent.find(iterator => iterator.page === currentPage) : null;
 
 
-        if (!currentPageData.titles) {
-            return <div className="text-neutral-200/80 flex flex-1 items-center justify-center w-full ">Loading...</div>;
+        if (!currentPageData || !currentPageData.titles) {
+            return <div className="text-neutral-200/80 flex flex-1 items-center justify-center w-full ">
+                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                Loading...
+            </div>;
         }
 
         // Add titles to combined content
@@ -195,17 +210,21 @@ const Book = () => {
         const renderContent = combinedContent.map((item, index) => {
             if (item.type === 'title') {
                 return (
-                    <div className={`w-full my-3 flex items-center justify-center text-center bg-neutral-700 rounded p-2 font-semibold text-neutral-300  whitespace-pre-line ${item.order === 0 ? "text-2xl font-bold" : " text-base"}`}>
+                    <div className={`w-full my-3 flex items-center justify-center text-center bg-sky-700 rounded p-2 font-semibold text-neutral-300  whitespace-pre-line ${item.order === 0 ? "text-2xl font-bold" : " text-base"}`}>
                         <h2 key={`title-${index}`}>{item.content}</h2>
                     </div>
                 );
             } else if (item.type === 'text') {
-                return <p key={`text-${index}`} className="my-4 indent-7 ">{parseReferences(item.content)}</p>;
+                return (
+                    <div className="rounded bg-neutral-700 text-neutral-300 p-2 shadow-lg mb-3 flex w-fit justify-center">
+                        <p key={`text-${index}`} className="px-1">{parseReferences(item.content)}</p>
+                    </div>
+                );
             } else if (item.type === 'evidence') {
                 return (
-                    <div key={`evidence-${index}`} className={`bg-sky-700 rounded text-sm md:text-base p-3 border-2 mb-2 border-sky-600`}>
+                    <div key={`evidence-${index}`} className={`bg-neutral-800 rounded shadow-lg text-sm md:text-base p-3 border my-3 border-neutral-900`}>
                         {Object.entries(item.content.lines).map(([lineKey, lineValue]) => (
-                            <p className="my-2 whitespace-pre-wrap" key={lineKey}>{parseReferences(lineValue)}</p>
+                            <p className=" whitespace-pre-wrap my-1" key={lineKey}>{parseReferences(lineValue)}</p>
                         ))}
                         {item.content.ref.length > 0 && (
                             <p>{parseReferences("[" + item.content.ref.join(', ') + "]")}</p>
@@ -216,10 +235,8 @@ const Book = () => {
 
                 const imageUrl = images(`./${item.no}.jpg`);
                 return (
-                    <div className=" flex flex-col flex-1 items-center justify-center w-full">
-                        <div className="text-neutral-200/80 w-full text-base flex justify-center">
-                            {item.text}
-                        </div>
+                    <div className=" flex flex-col flex-1 items-center justify-center w-full px-1">
+
                         <div className="rounded shadow-lg flex justify-center">
 
                             <img
@@ -228,6 +245,11 @@ const Book = () => {
                                 className=" object-center"
                             />
                         </div>
+                        {item.text && <div className="text-sky-200/70 w-full text-base flex justify-center">
+                            <div className="p-2">
+                                {item.text}
+                            </div>
+                        </div>}
                     </div>
                 );
             } else {
@@ -247,13 +269,13 @@ const Book = () => {
     };
 
     return (
-        <div className="flex flex-col justify-start h-screen bg-sky-800">
+        <div className="flex flex-col justify-start h-screen bg-sky-950">
             {renderBookContent()}
             <div className="w-full flex z-20">
                 <div className="flex w-full items-center justify-between p-2">
                     <button onClick={prevPage}
                         className="w-28 text-neutral-300 px-2 py-1 rounded mr-2 flex justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                         </svg>
                     </button>
@@ -264,7 +286,7 @@ const Book = () => {
                     </div>
                     <button onClick={nextPage}
                         className="w-28 text-neutral-300 px-2 py-1 rounded ml-2 flex justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3" />
                         </svg>
                     </button>
