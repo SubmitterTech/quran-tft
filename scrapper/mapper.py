76c3c3a6564c06ac777de41dcd35dbb0e61dc91c
Yunusemre Şentürk <se.yunusemre@gmail.com>
2023-12-23 21:29:21 +0300

    More sophisticated mapping for search and relation purposes
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- scrapper/mapper.py
+++ scrapper/mapper.py
@@ -1,48 +1,113 @@
 import json
 import re
+from collections import defaultdict
 
-def extract_relations(text):
-    """
-    Extract relations from the provided text.
-    """
-    # Regular expression to match the sura:verse format
-    regex = r'(\d+:\d+(?:-\d+|,\d+)?)'
-    
-    # Split the text by semicolon to handle different relation groups
-    groups = text.split(';')
-    relation_groups = []
 
-    for group in groups:
-        # Find all matches in the group
-        found = re.findall(regex, group)
-        if found:
-            relation_groups.append(found)
+def parse_index(entries):
+    dictionary = defaultdict(lambda: defaultdict(dict))
+    current_main_key = ''
+    current_sub_key = ''
+    current_sub_entries = defaultdict(str)
 
-    return relation_groups
+    for entry in entries:
+        entry = entry.strip()
 
-# Load the json file
+        if len(entry) == 0:
+            continue # Ignore empty line
+        if re.match(r"index", entry, flags=re.IGNORECASE):
+            continue  # Ignore index name at page corner
+        if re.match(r"^\d\d\d", entry, flags=re.IGNORECASE):
+            if len(entry) == 3 and int(entry) > 488:
+                continue  # Ignore page numbers
+
+        if re.match(r'^[A-Z]$', entry):
+            if current_main_key and current_sub_entries:
+                dictionary[current_main_key].update(current_sub_entries)
+                current_sub_entries = defaultdict(str)
+
+            current_main_key = entry
+            current_sub_key = ''
+        else:
+            if entry.startswith(current_main_key) and (not current_sub_key or entry != current_sub_key):
+                if current_sub_key:
+                    dictionary[current_main_key][current_sub_key] = current_sub_entries[current_sub_key]
+
+                sub_entry_match = re.match(r"([A-Za-z, ]+)(\d.*)", entry)
+                
+                if sub_entry_match:
+                    current_sub_key, current_references = sub_entry_match.groups()
+                    current_sub_entries[current_sub_key] = current_references.strip()
+                else:
+                    split_point = re.search(r'\d', entry)
+                    if split_point:
+                        index = split_point.start()
+                        current_sub_key = entry[:index].strip()
+                        current_references = entry[index:].strip()
+                        current_sub_entries[current_sub_key] = current_references.strip()
+                    else:
+                        current_sub_key = entry
+                        current_references = ''
+
+            elif current_sub_key:
+                if re.search(r"[A-Za-z]", entry):
+                    split_point = re.search(r'\d', entry)
+                    inner_key = entry
+                    if split_point:
+                        index = split_point.start()
+                        inner_key = entry[:index].strip()
+                        value = entry[index:].strip()
+                        if isinstance(current_sub_entries[current_sub_key], str):
+                            current_sub_entries[current_sub_key] = {inner_key: value}
+                        else:
+                            current_sub_entries[current_sub_key][inner_key] = value
+                    else:
+                        if re.search(r"see", entry):
+                            print(entry)
+                        if isinstance(current_sub_entries[current_sub_key], str):
+                            current_sub_entries[current_sub_key] = {entry: ''}
+                        else:
+                            current_sub_entries[current_sub_key][entry] = ''
+                else:
+                    if current_sub_entries[current_sub_key]:
+                        if isinstance(current_sub_entries[current_sub_key], dict):
+                            last_key = list(current_sub_entries[current_sub_key])[-1]
+                            if len(current_sub_entries[current_sub_key][last_key]) > 0:
+                                if current_sub_entries[current_sub_key][last_key][-1] == ";": 
+                                    current_sub_entries[current_sub_key][last_key] += " " + entry
+                                elif current_sub_entries[current_sub_key][last_key][-1] == "-":
+                                    current_sub_entries[current_sub_key][last_key] += entry
+                                else:
+                                    current_sub_entries[current_sub_key][last_key] += "; " + entry
+                            else:
+                                current_sub_entries[current_sub_key][last_key] +=  entry
+                        else:
+                            if current_sub_entries[current_sub_key][-1] == ";": 
+                                current_sub_entries[current_sub_key] += " " + entry
+                            elif current_sub_entries[current_sub_key][-1] == "-":
+                                current_sub_entries[current_sub_key] += entry
+                            elif current_sub_entries[current_sub_key][-1] == ",":
+                                current_sub_entries[current_sub_key] += entry
+                            else:
+                                current_sub_entries[current_sub_key] += "; " + entry
+
+                    else:
+                        current_sub_entries[current_sub_key] += entry
+
+    if current_main_key and current_sub_entries:
+        dictionary[current_main_key].update(current_sub_entries)
+
+    return {k: dict(v) for k, v in dictionary.items()}
+
 with open('index.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+    index_json = json.load(file)
 
-# Process each page
-mapped_relations = {}
-for page in data:
-    # Extract relations from text
-    page_relation_groups = extract_relations(page['text'])
+all_entries = []
+for page in index_json:
+    all_entries.extend(page['text'].split('\n'))
+    # if page['page'] == 517:
+    #     break
 
-    # Iterate through each group and store the relations
-    for group in page_relation_groups:
-        for relation in group:
-            if relation not in mapped_relations:
-                mapped_relations[relation] = set()
+parsed_index = parse_index(all_entries)
 
-            # Add other related verses in the group excluding the current one
-            mapped_relations[relation].update(set(group) - {relation})
-
-# Convert sets to lists for JSON serialization
-for key, value in mapped_relations.items():
-    mapped_relations[key] = list(value)
-
-# Write the relations to a JSON file
-with open('map.json', 'w', encoding='utf-8') as out_file:
-    json.dump(mapped_relations, out_file, ensure_ascii=False, indent=4)
+with open('map.json', 'w', encoding='utf-8') as f:
+    json.dump(parsed_index, f, indent=4, ensure_ascii=False)
