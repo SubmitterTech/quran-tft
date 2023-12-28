181952af36d8c5037fabf0d092b0f34160a230e5
Yunusemre Şentürk <se.yunusemre@gmail.com>
2023-12-28 19:10:36 +0300

    Enhance change language mechanism, update Cover
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/components/Cover.js
+++ app/src/components/Cover.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import splashData from '../assets/cover.json';
+import React, { useState } from 'react';
 import '../assets/css/Splash.css';
+import languages from '../assets/languages.json';
 
-const Cover = ({onCoverSeen}) => {
+const Cover = ({ onCoverSeen, coverData, lang, onChangeLanguage }) => {
     // Split the text into lines
-    const lines = splashData.text.split('\n');
+
+    const [show19, setShow19] = useState(true)
 
     const handleTap = async (e) => {
-        onCoverSeen();
+        setShow19(!show19);
     };
     // Forward to the source
     const openPdf = () => {
@@ -15,40 +16,78 @@ const Cover = ({onCoverSeen}) => {
     };
 
     return (
-        <div className="splash-screen h-screen flex flex-col w-full p-1 justify-between text-center bg-sky-700 text-neutral-200">
-            <div className="flex flex-col w-full space-y-2 md:space-y-3 mt-4">
-                <h1 className="text-6xl font-extrabold">{lines[0]}</h1>
-                <h2 className="text-3xl font-bold">{lines[2]}</h2>
-                <h2 className="text-3xl font-bold">{lines[3]}</h2>
-                <h2 className="text-3xl font-bold">{lines[4]}</h2>
-                <p className="text-lg">{lines[5]}</p>
-            </div>
-            <button className="flex flex-col items-center justify-end"
-                onClick = {handleTap}>
-                {/* 19 lines for animated splash, starting from the bottom */}
-                {Array.from({ length: 19 }).map((_, index) => (
-                    <div
-                        key={index}
-                        className="splash-line bg-neutral-100 h-1 my-1.5 md:h-2 md:my-4"
-                        style={{
-                            animationDelay: `${0.5 * (18 - index)}s`, // Animation delay starts from the longest (bottom) line
-                            width: `${91 - 2 * (18 - index)}%` // Width decreases from bottom to top
-                        }}
-                    >
+        <div className="splash-screen h-screen flex flex-col w-full p-1 justify-between  text-center bg-sky-800 text-neutral-300">
 
-                    </div>
-                ))}
-            </button>
-
-            <div
-                className="text-sky-400 cursor-pointer"
-                onClick={openPdf}
-            >
-                <p>{lines[7]}</p>
-                <p>{lines[8]}</p>
-                <p>{lines[9]}</p>
+            <div className="w-full h-full flex items-center">
+                <div className="flex flex-col w-full space-y-2 md:space-y-3 mt-4 ">
+                    <h1 className="text-6xl font-extrabold">{coverData.quran}</h1>
+                    <h2 className="text-4xl font-bold whitespace-pre">{coverData.title}</h2>
+                    <h2 className="text-lg font-bold whitespace-pre">{coverData.version}</h2>
+                </div>
             </div>
-            <p>{lines[11] + ' 1992'}</p>
+            <div className="w-full h-full">
+                <div className={`flex flex-col flex-1 items-center justify-end ${show19 ? "opacity-100 " : "opacity-0 h-0"} transition-all duration-1000 ease-linear `}
+                    onClick={handleTap}>
+                    {/* 19 lines for animated splash, starting from the bottom */}
+                    {Array.from({ length: 19 }).map((_, index) => (
+                        <div
+                            key={index}
+                            className="splash-line bg-white h-1 my-1.5 md:h-2 md:my-4"
+                            style={{
+                                animationDelay: `${0.5 * (18 - index)}s`, // Animation delay starts from the longest (bottom) line
+                                width: `${91 - 2 * (18 - index)}%` // Width decreases from bottom to top
+                            }}
+                        >
+
+                        </div>
+                    ))}
+                </div>
+                <div className={` flex flex-col max-h-full justify-center flex-1 p-4 space-y-5 items-center bg-sky-900 rounded mx-4 ${show19 ? "opacity-0 h-0 pointer-events-none" : "opacity-100"} transition-all duration-1000 ease-linear `}>
+                    {Object.keys(languages).map((key) => {
+                        if (key) {
+                            const isSelectedLanguage = lang === key;
+                            return (
+                                <div
+                                    key={key}
+                                    className={`flex p-2 shadow-md rounded bg-neutral-800 justify-center ${isSelectedLanguage ? "ring-1 ring-offset-4 ring-sky-400 ring-offset-sky-900" : ""}`}
+                                    onClick={() => onChangeLanguage(key)}>
+                                    <input
+                                        type="radio"
+                                        name="theme"
+                                        value={key}
+                                        checked={isSelectedLanguage}
+                                        onChange={(e) => onChangeLanguage(e.target.value)}
+                                        className="hidden"
+                                    />
+                                    {languages[key]}
+                                </div>
+                            );
+                        }
+                        return <></>;
+                    })}
+                </div>
+                {show19 || <div className="text-neutral-200 flex items-center justify-center w-full mt-3">
+                    <button onClick={() => onCoverSeen()}
+                        className={` px-2 mb-1 rounded ml-2 flex justify-center`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-12 h-12`}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3" />
+                        </svg>
+                    </button>
+                </div>}
+            </div>
+
+            <div className="w-full mb-5 flex flex-col space-y-3 h-full justify-center">
+                <div
+                    className="text-sky-400 cursor-pointer"
+                    onClick={openPdf}
+                >
+                    <p className="whitespace-pre">{coverData.translated}</p>
+                    <p className="whitespace-pre">{coverData.author}</p>
+
+                </div>
+                <p className="whitespace-pre">{coverData.edition + "  1992"}</p>
+            </div>
+
         </div>
     );
 };
--- app/src/pages/Root.js
+++ app/src/pages/Root.js
@@ -5,10 +5,15 @@ import introductionContent from '../assets/introduction.json';
 import quranData from '../assets/qurantft.json';
 import appendicesContent from '../assets/appendices.json';
 import application from '../assets/application.json';
+import cover from '../assets/cover.json';
+import colors from '../assets/colors.json';
 
 
 function Root() {
     const [showCover, setShowCover] = useState(localStorage.getItem("qurantft-pn") ? false : true);
+    const [coverData, setCoverData] = useState(cover);
+    const [lang, setLang] = useState(localStorage.getItem("lang") ? localStorage.getItem("lang") : "en");
+
     const [translation, setTranslation] = useState(null);
     const [translationApplication, setTranslationApplication] = useState(application);
     const [translationIntro, setTranslationIntro] = useState(introductionContent);
@@ -18,80 +23,27 @@ function Root() {
 
     const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "sky");
 
-    const colors = {
-        "light": {
-            "text-background": "bg-neutral-300",
-            "app-background": "bg-neutral-200",
-            "notes-background": "bg-neutral-200",
-            "base-background": "bg-neutral-100",
-            "verse-detail-background": "bg-neutral-100",
-            "encrypted-background": "bg-neutral-200",
-            "relation-background": "bg-neutral-200",
-            "table-title-text": "text-neutral-700",
-            "border": "border-neutral-900",
-            "text": "text-neutral-900",
-            "app-text": "text-neutral-800",
-            "page-text": "text-neutral-900/50",
-            "log-text": "text-neutral-800/80",
-            "verse-border": "border-neutral-800/80",
-            "ring": "ring-neutral-800/80",
-            "focus-ring": "focus:ring-neutral-800/80",
-            "focus-text": "focus:text-neutral-800"
-        },
-        "dark": {
-            "text-background": "bg-neutral-700",
-            "app-background": "bg-neutral-800",
-            "notes-background": "bg-neutral-800",
-            "base-background": "bg-neutral-900",
-            "verse-detail-background": "bg-neutral-900",
-            "encrypted-background": "bg-neutral-800",
-            "relation-background": "bg-neutral-800",
-            "table-title-text": "text-neutral-300",
-            "border": "border-neutral-200",
-            "text": "text-neutral-200",
-            "app-text": "text-neutral-300",
-            "page-text": "text-neutral-200/50",
-            "log-text": "text-neutral-300/80",
-            "verse-border": "border-neutral-300/80",
-            "ring": "ring-neutral-300/80",
-            "focus-ring": "focus:ring-neutral-200/80",
-            "focus-text": "focus:text-neutral-200"
-        },
-        "sky": {
-            "text-background": "bg-sky-800",
-            "app-background": "bg-sky-950",
-            "notes-background": "bg-neutral-700",
-            "base-background": "bg-neutral-800",
-            "verse-detail-background": "bg-neutral-800",
-            "encrypted-background": "bg-neutral-700",
-            "relation-background": "bg-neutral-700",
-            "table-title-text": "text-neutral-300",
-            "border": "border-sky-100",
-            "text": "text-neutral-100",
-            "app-text": "text-neutral-300",
-            "page-text": "text-sky-100/50",
-            "log-text": "text-neutral-300/80",
-            "verse-border": "border-sky-500/80",
-            "ring": "ring-sky-500/80",
-            "focus-ring": "focus:ring-neutral-200/80",
-            "focus-text": "focus:text-neutral-200"
-        }
-    }
-
     const onChangeTheme = (theme) => {
         setTheme(theme);
         localStorage.setItem("theme", theme)
     };
 
-    const lang = navigator.language || navigator.userLanguage;
-
     useEffect(() => {
-        loadTranslation(lang);
+        if (!lang.toLowerCase().includes("en")) {
+            loadTranslation(lang);
+            localStorage.setItem("lang", lang)
+        } else {
+            setTranslation(null);
+            setCoverData(cover);
+        }
     }, [lang]);
 
     const loadTranslation = async (language) => {
         try {
             const translatedQuran = await import(`../assets/translations/${language}/quran_${language}.json`)
+                .catch(() => null);
+
+            const translatedCover = await import(`../assets/translations/${language}/cover_${language}.json`)
                 .catch(() => null);
 
             const translatedIntro = await import(`../assets/translations/${language}/introduction_${language}.json`)
@@ -105,6 +57,12 @@ function Root() {
             } else {
                 console.error('Quran Translation file not found for language:', language);
                 setTranslation(null);
+            }
+
+            if (translatedCover) {
+                setCoverData(translatedCover.default);
+            } else {
+                console.error('Cover Translation file not found for language:', language);
             }
 
             if (translatedIntro) {
@@ -139,7 +97,7 @@ function Root() {
 
     return (
         <div className={`Root select-none flex flex-col h-screen`}>
-            {showCover && <Cover onCoverSeen={hideCover} />}
+            {showCover && <Cover onCoverSeen={hideCover} coverData={coverData} lang={lang} onChangeLanguage={setLang} />}
             {!showCover && <Book
                 onChangeTheme={onChangeTheme}
                 colors={colors} theme={theme}
