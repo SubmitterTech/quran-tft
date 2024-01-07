ef857a3b83964345548a048e93d25ce77d6e303c
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-01-07 18:21:48 +0300

    Introduce sandbox small react app for manual verification of encrypted quran data work
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

---
+++ sandbox/src/App.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from 'react';
+import quranData from './assets/qurantft.json';
+import './App.css';
+
+function App() {
+
+  const [selectedSura, setSelectedSura] = useState(null);
+  const [selectedVerse, setSelectedVerse] = useState(null);
+  const [selectedWord, setSelectedWord] = useState(null);
+
+
+  const [quranMap, setQuranMap] = useState({});
+
+  useEffect(() => {
+    let qmap = {};
+    Object.values(quranData).forEach((page) => {
+      Object.entries(page.sura).forEach(([sno, content]) => {
+        if (!qmap[sno]) { qmap[sno] = {}; }
+        Object.entries(content.encrypted).forEach(([vno, verse]) => {
+          qmap[sno][vno] = verse;
+        });
+      });
+    });
+    setQuranMap(qmap);
+  }, []);
+
+  // const countLetterInSura = async (sura, l) => {
+  //   let cnt = 0;
+
+  //   // Convert the task to asynchronous using Promise
+  //   await new Promise(resolve => {
+  //     Object.values(quranMap[sura]).forEach((verse) => {
+  //       // Count the occurrences of the letter in each verse
+  //       for (const char of verse) {
+  //         if (char === l) {
+  //           cnt++;
+  //         }
+  //       }
+  //       // console.log(verse, l, cnt)
+
+  //     });
+  //     resolve();
+  //   });
+
+  //   return cnt;
+  // };
+
+  const countWordOccurrences = () => {
+    if (!selectedWord) return 0;
+    let count = 0;
+    Object.values(quranMap).forEach(verses => {
+      Object.values(verses).forEach(verse => {
+        count += verse.split(" ").filter(word => word.trim() === selectedWord).length;
+      });
+    });
+    return count;
+  };
+
+  const handleSelectedVerse = (s, v) => {
+    if (selectedSura === s && selectedVerse === v) {
+      setSelectedSura(null)
+      setSelectedVerse(null)
+    } else {
+      setSelectedSura(s)
+      setSelectedVerse(v)
+    }
+  }
+
+  const handleSelectedWord = (w) => {
+    console.log(w)
+    if (selectedWord === w) {
+      setSelectedWord(null)
+    } else {
+      setSelectedWord(w)
+    }
+  }
+
+  const filterVersesByWord = (verses) => {
+    if (!selectedWord) return verses;
+    return Object.fromEntries(
+      Object.entries(verses).filter(([_, verseText]) => 
+        verseText.includes(selectedWord))
+    );
+  };
+
+  return (
+    <div className="App w-screen h-screen bg-neutral-500 text-neutral-100 flex ">
+      <div className="flex flex-col w-full overflow-auto h-full">
+        {selectedWord && (
+          <div className="rounded shadow-lg p-2 text-start m-2 bg-purple-700">
+            Filter: {selectedWord} (Occurrences: {countWordOccurrences()})
+          </div>
+        )}
+
+        {Object.entries(quranMap).map(([sura, verses]) => (
+          <div key={sura} className="flex flex-col space-y-1 m-2">
+            {/* <div className="p-2 rounded bg-rose-800 text-start sticky top-0 shadow-lg">
+              Sura: {sura}
+            </div> */}
+            {Object.entries(filterVersesByWord(verses)).map(([vno, verseText]) => (
+              <div key={vno} className="text-start w-full flex justify-between space-x-3">
+                <div className="w-16 p-2 rounded shadow-lg bg-sky-600 text-end">{sura}:{vno}</div>
+                <div
+                  onClick={() => handleSelectedVerse(sura, vno)}
+                  className={`w-full p-2 rounded shadow-lg cursor-pointer ${selectedSura === sura && selectedVerse === vno ? "bg-neutral-700" : "bg-neutral-800"}`}
+                  dir="rtl">
+                  {verseText}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col w-full overflow-auto h-full ">
+        <div className="w-full p-1">
+
+          <div className="p-2 bg-orange-700 mb-4 mt-1 rounded shadow-lg">
+            {selectedSura}:{selectedVerse}
+          </div>
+
+          {selectedVerse &&
+            <div className="flex flex-col space-y-2">
+              <div
+                key={"selected_" + selectedSura + ":" + selectedVerse}
+                className="w-full p-2 rounded shadow-lg bg-neutral-800 text-start"
+                dir="rtl">
+                {quranMap[selectedSura][selectedVerse]}
+              </div>
+              <div dir="rtl" className={`w-full flex overflow-y-auto items-center justify-start pb-3  rounded `}>
+                {quranMap[selectedSura][selectedVerse].split(" ").map((word, index) => (
+                  <p
+                    onClick={() => handleSelectedWord(word.trim())}
+                    key={selectedSura + selectedVerse + index + word}
+                    className={`p-2 shadow-md rounded text-start ml-1 cursor-pointer ${selectedWord === word.trim() ? "bg-neutral-700 " : "bg-neutral-800 "}`}
+                    dir="rtl">
+                    {word}
+                  </p>
+                ))}
+              </div>
+            </div>
+
+
+          }
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
