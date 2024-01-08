9b6a166744d3c84d1318827a4ab91cb396ddc289
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-01-08 16:03:47 +0300

    sandbox: revert batch loading mechanism
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import quranData from './assets/qurantft.json';
 import './App.css';
 
@@ -7,15 +7,9 @@ function App() {
   const [selectedSura, setSelectedSura] = useState(null);
   const [selectedVerse, setSelectedVerse] = useState(null);
   const [selectedWord, setSelectedWord] = useState(null);
-  const [displayedVerses, setDisplayedVerses] = useState({});
-  const [currentBatchIndex, setCurrentBatchIndex] = useState(0);
-  const batchSize = 19;
-  const observer = useRef();
 
 
   const [quranMap, setQuranMap] = useState({});
-
-
 
   useEffect(() => {
     let qmap = {};
@@ -29,66 +23,6 @@ function App() {
     });
     setQuranMap(qmap);
   }, []);
-
-  const filterVersesByWord = useCallback((verses) => {
-    if (!selectedWord) return verses;
-    return Object.fromEntries(
-      Object.entries(verses).filter(([_, verseText]) =>
-        verseText.includes(selectedWord))
-    );
-  }, [selectedWord]);
-
-  const loadMoreVerses = useCallback(() => {
-    if (currentBatchIndex * batchSize >= Object.keys(quranMap).length) {
-      // All verses are loaded, no more to load
-      return;
-    }
-
-    const newBatch = {};
-    const verseEntries = Object.entries(quranMap); // Use quranMap directly
-
-    for (let i = currentBatchIndex * batchSize;
-      i < Math.min((currentBatchIndex + 1) * batchSize, verseEntries.length);
-      i++) {
-      const [sura, verses] = verseEntries[i];
-      newBatch[sura] = verses;
-    }
-
-    setDisplayedVerses(prevVerses => ({ ...prevVerses, ...newBatch }));
-    setCurrentBatchIndex(prevIndex => prevIndex + 1);
-  }, [quranMap, currentBatchIndex]);
-
-  const lastVerseElementRef = useCallback(node => {
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && Object.keys(quranMap).length > currentBatchIndex * batchSize) {
-        loadMoreVerses();
-      }
-    });
-    if (node) observer.current.observe(node);
-  }, [loadMoreVerses, currentBatchIndex, quranMap]);
-
-  const handleScroll = useCallback(() => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-    loadMoreVerses();
-  }, [loadMoreVerses]);
-
-  useEffect(() => {
-    loadMoreVerses();
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [loadMoreVerses, handleScroll]);
-
-  useEffect(() => {
-    // Call loadMoreVerses when quranMap is ready
-    if (Object.keys(quranMap).length > 0) {
-      loadMoreVerses();
-    }
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [quranMap, loadMoreVerses, handleScroll]); // Depend on quranMap
-
-
 
   // const countLetterInSura = async (sura, l) => {
   //   let cnt = 0;
@@ -141,7 +75,13 @@ function App() {
     }
   }
 
-
+  const filterVersesByWord = (verses) => {
+    if (!selectedWord) return verses;
+    return Object.fromEntries(
+      Object.entries(verses).filter(([_, verseText]) =>
+        verseText.includes(selectedWord))
+    );
+  };
 
   const lightMatchWords = useCallback((verse) => {
     if (!selectedWord) {
@@ -162,43 +102,42 @@ function App() {
     return <div dir="rtl">{words}</div>;
   }, [selectedWord]);
 
+  
 
   return (
     <div className="App w-screen h-screen bg-neutral-400 text-neutral-100 flex ">
-      <div className="verses-main-list flex flex-col w-full overflow-auto mb-2 mt-2">
+      <div className="flex flex-col w-full overflow-auto mb-2 mt-2">
+
         <div className="rounded shadow-lg p-2 text-start mx-2 mb-3 bg-fuchsia-800 sticky top-0 flex justify-between">
-          <div>Filter: {selectedWord ? selectedWord : "N / A"}</div>
-          <div>{selectedWord && `Occurrences: ${countWordOccurrences()}`}</div>
+          <div >
+            Filter: {selectedWord ? selectedWord : "N / A"}
+          </div>
+          <div>
+            {selectedWord && `Occurrences: ${countWordOccurrences()}`}
+          </div>
+
         </div>
 
-        {Object.entries(displayedVerses).map(([sura, verses], index) => (
+
+        {Object.entries(quranMap).map(([sura, verses]) => (
           <div key={sura} className="flex flex-col space-y-1 mx-2 my-1">
-            {Object.entries(filterVersesByWord(verses)).map(([vno, verseText], verseIndex, verseArray) => {
-              const isLastVerse = index === Object.keys(displayedVerses).length - 1 && verseIndex === verseArray.length - 1;
-              return (
+            {/* <div className="p-2 rounded bg-rose-800 text-start sticky top-0 shadow-lg">
+              Sura: {sura}
+            </div> */}
+            {Object.entries(filterVersesByWord(verses)).map(([vno, verseText]) => (
+              <div key={vno} className="text-start w-full flex justify-between space-x-3">
+                <div className="w-20 p-2 rounded shadow-lg bg-sky-600 text-end">{sura}:{vno}</div>
                 <div
-                  ref={isLastVerse ? lastVerseElementRef : null}
-                  key={vno}
-                  className="text-start w-full flex justify-between space-x-3">
-                  <div className="w-20 p-2 rounded shadow-lg bg-sky-600 text-end">{sura}:{vno}</div>
-                  <div
-                    onClick={() => handleSelectedVerse(sura, vno)}
-                    className={`w-full p-2 rounded shadow-lg cursor-pointer ${selectedSura === sura && selectedVerse === vno ? "bg-neutral-700" : "bg-neutral-800"}`}
-                    dir="rtl">
-                    {lightMatchWords(verseText)}
-                  </div>
+                  onClick={() => handleSelectedVerse(sura, vno)}
+                  className={`w-full p-2 rounded shadow-lg cursor-pointer ${selectedSura === sura && selectedVerse === vno ? "bg-neutral-700" : "bg-neutral-800"}`}
+                  dir="rtl">
+                  {lightMatchWords(verseText)}
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         ))}
-        {currentBatchIndex * batchSize < Object.keys(quranMap).length && (
-          <div ref={lastVerseElementRef} className="loading-indicator">
-            Loading more verses...
-          </div>
-        )}
       </div>
-
 
       <div className="flex flex-col w-full overflow-auto h-full ">
         <div className="w-full p-1">
