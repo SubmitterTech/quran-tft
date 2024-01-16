c3a2cd119e5acbbec51c0608d5858320815cf425
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-01-16 13:59:44 +0300

    Better UI looking and more simple listing with titles
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/pages/Leaf.js
+++ app/src/pages/Leaf.js
@@ -8,6 +8,7 @@ function Leaf() {
     const [quranmap, setQuranmap] = useState({});
 
     const [verseList, setVerseList] = useState({});
+    const [titleList, setTitleList] = useState({});
 
     useEffect(() => {
         let qm = {};
@@ -21,6 +22,10 @@ function Leaf() {
                 Object.entries(content.verses).forEach(([verse, text]) => {
                     qm[sura][verse] = text;
                 });
+
+                Object.entries(content.titles).forEach(([title, text]) => {
+                    qm[sura]["t" + title] = text;
+                });
             });
         });
         setQuranmap(qm);
@@ -29,40 +34,37 @@ function Leaf() {
     useEffect(() => {
 
         let vl = {};
-        params.split(",").forEach((key) => {
-            const [s,v] = key.trim().split(":")
+        let tl = {};
+        params.split(";").forEach((key) => {
+            const [s, v] = key.trim().split(":")
             if (quranmap && quranmap[s]) {
                 vl[key] = quranmap[s][v]
+                if (quranmap[s]["t" + v]) {
+                    tl[key] = quranmap[s]["t" + v]
+                }
             }
         });
 
         setVerseList(vl);
-
-        // Update the title
-        const initialKey = params.split(",")[0]
-        document.title = initialKey;
-
-        // Update meta description
-        let metaDescription = document.querySelector('meta[name="description"]');
-        if (!metaDescription) {
-            metaDescription = document.createElement('meta');
-            metaDescription.setAttribute('name', 'description');
-            document.head.appendChild(metaDescription);
-        }
-
-        // Set the content of the meta description
-        if (quranmap && quranmap[initialKey.split(":")[0]]) {
-            metaDescription.setAttribute('content', quranmap[initialKey.split(":")[0]][initialKey.split(":")[1]]);
-        }
+        setTitleList(tl);
 
     }, [params, quranmap]);
 
     return (
-        <div className={`w-screen h-screen bg-neutral-900 p-2`}>
-            <div className={`w-full flex flex-col space-y-2 overflow-auto`}>
+        <div className={`select-text w-screen h-screen bg-gradient-to-r from-sky-400 to-cyan-400 pl-2 py-2 flex flex-col justify-center`}>
+            <div className={`w-full flex flex-col overflow-auto pr-2`}>
                 {Object.entries(verseList).map(([key, text]) => (
-                    <div className={`rounded shadow-lg text-neutral-100 bg-neutral-700 text-justify hyphens-auto p-1.5`}>
-                        {key} {` `} {text}
+                    <div className={` text-neutral-100 text-justify hyphens-auto p-1.5`}>
+                        <div className={`text-center hyphens-auto p-1.5 text-neutral-900`}>
+                            {key}
+                        </div>
+                        {titleList[key] &&
+                            <div className={`rounded shadow-md bg-sky-800/90 text-justify hyphens-auto py-1.5 px-2 mb-2`}>
+                                {titleList[key]}
+                            </div>}
+                        <div className={`rounded shadow-md bg-neutral-900/90 text-justify hyphens-auto py-1.5 px-2`}>
+                            {text}
+                        </div>
                     </div>
                 ))}
             </div>
