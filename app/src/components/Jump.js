a42a69c0f1cf26dd11b9108ec4034d6094d2d2ce
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-07-06 18:03:04 +0300

    Adjust theme picking border thickness and Jump menu shadow thickness
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/components/Jump.js
+++ app/src/components/Jump.js
@@ -204,7 +204,7 @@ const Jump = ({ onChangeLanguage, suraNames, onChangeTheme, colors, theme, trans
                             className="hidden"
                         />
                         <span
-                            className={`flex items-center justify-center rounded border flex-1 ${localTheme === theme ? `${colors[theme]["matching-border"]}` : "border-gray-400"} h-20`}
+                            className={`flex items-center justify-center rounded flex-1 ${localTheme === theme ? `border-2 ${colors[theme]["matching-border"]}` : "border border-gray-400"} h-20`}
                             style={{ backgroundColor: color }}>
                             {localTheme === theme &&
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${colors[theme]["matching-text"]} w-9 h-9`}>
@@ -272,7 +272,7 @@ const Jump = ({ onChangeLanguage, suraNames, onChangeTheme, colors, theme, trans
                     </select>
                 </div>
                 <div className={` w-full md:w-2/3 lg:w-1/2 2xl:w-1/3 px-2 flex flex-col transition-all duration-200 ease-linear mb-7 `}>
-                    <div className={`shadow-[rgba(125,211,252,0.4)_0px_7px_15px_15px] transition-colors duration-200 ease-linear flex flex-col items-center justify-center ${colors[theme]["app-background"]} rounded  w-full `}>
+                    <div className={`shadow-[rgba(125,211,252,0.4)_0px_2px_10px_10px] transition-colors duration-200 ease-linear flex flex-col items-center justify-center ${colors[theme]["app-background"]} rounded  w-full `}>
                         <div className={`w-full pt-2`}>
                             <div className={`w-full flex space-x-2 px-2`}>
                                 <div
@@ -494,9 +494,7 @@ const Jump = ({ onChangeLanguage, suraNames, onChangeTheme, colors, theme, trans
                                         (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-12 h-12`}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z" />
                                         </svg>
-
                                         )}
-
                                 </div>
                                 <div className={`flex ${colors[theme]["page-text"]} text-sm items-center justify-center pb-1`}>
                                     {translationApplication?.color}
@@ -504,7 +502,6 @@ const Jump = ({ onChangeLanguage, suraNames, onChangeTheme, colors, theme, trans
                             </button>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
