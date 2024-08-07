e93e817b7ea578b32985d80433fae98b7c1c9fe0
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-08-07 12:29:42 +0300

    Adjust bookmarks button width in Jump menu
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/components/Jump.js
+++ app/src/components/Jump.js
@@ -284,13 +284,13 @@ const Jump = ({ onChangeLanguage, suraNames, onChangeTheme, colors, theme, trans
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                         </svg>
                                     </button>
-                                    <div className={`flex text-left items-center ml-3 ${colors[theme]["matching-text"]} text-xl`}>
+                                    <div className={`flex items-center ml-3 ${colors[theme]["matching-text"]} text-xl`}>
                                         {translationApplication.search}<span className={`${colors[theme]["text"]}`}>{"..."}</span>
                                     </div>
                                 </div>
                                 <button
                                     onClick={toggleBookmark}
-                                    className={`flex flex-col w-1/4 items-center justify-between pt-2 rounded ${colors[theme]["text"]} ${colors[theme]["text-background"]}`}>
+                                    className={`flex flex-col w-1/3 items-center justify-between pt-2 rounded ${colors[theme]["text"]} ${colors[theme]["text-background"]}`}>
                                     <div className={`flex justify-center`}>
                                         {showBookmarks ? (
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-11 h-11`}>
