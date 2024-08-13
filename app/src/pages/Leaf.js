5c1fd9f203b589230a626b1296031be3f5c5b524
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-08-13 16:20:42 +0300

    Enhance Leaf to support several formulas and add button to return to the digital quran
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/pages/Leaf.js
+++ app/src/pages/Leaf.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import defaultQuran from '../assets/qurantft.json'; // Import the default Quran data
+import { useParams, useLocation, Link } from 'react-router-dom';
+import defaultQuran from '../assets/qurantft.json';
 import { mapQuran } from '../utils/Mapper';
 
 const Leaf = () => {
-    const { lang = process.env.REACT_APP_DEFAULT_LANG || 'en', params } = useParams();
+    const { lang = process.env.REACT_APP_DEFAULT_LANG || 'en', params } = useParams();
     const [quranmap, setQuranmap] = useState({});
     const [verseList, setVerseList] = useState({});
     const [titleList, setTitleList] = useState({});
-
+    const [uf, setUf] = useState(false);
+    const loc = useLocation();
     useEffect(() => {
         // Function to process and set Quran data
         const processQuranData = (quranData) => {
@@ -35,73 +36,100 @@ const Leaf = () => {
     useEffect(() => {
         let vl = {};
         let tl = {};
-    
-        params.split(";").forEach((param) => {
-            const trimmedParam = param.trim();
-            if (!trimmedParam) {
-                return;
-            }
-    
-            const [surah, versesString] = trimmedParam.split(":");
-            if (!versesString) {
-                return;
-            }
-    
-            const verseParts = versesString.split(",").filter(versePart => versePart.trim() !== "");
-    
-            verseParts.forEach((versePart) => {
-                if (versePart.includes("-")) {
-                    // Handling range of verses
-                    const [start, end] = versePart.split("-").map(Number);
-                    if (!isNaN(start) && !isNaN(end)) {
-                        for (let verse = start; verse <= end; verse++) {
-                            const key = `${surah}:${verse}`;
-                            addVerseToCollection(key, surah, verse);
-                        }
+        if (params && params.match(/^\d+:/)) {
+            params.split(";").forEach((param) => {
+                const trimmedParam = param.trim();
+                if (!trimmedParam) {
+                    return;
+                }
+
+                const [surah, versesString] = trimmedParam.split(":");
+                if (versesString === undefined || versesString.trim() === "") {
+                    if (quranmap && quranmap[surah]) {
+                        Object.keys(quranmap[surah]).forEach((verseKey) => {
+                            if (!verseKey.startsWith("t")) {
+                                const key = `${surah}:${verseKey}`;
+                                addVerseToCollection(key, surah, verseKey);
+                            }
+                        });
                     }
                 } else {
-                    // Handling a single verse
-                    const verse = Number(versePart);
-                    if (!isNaN(verse)) {
-                        const key = `${surah}:${verse}`;
-                        addVerseToCollection(key, surah, verse);
-                    }
+                    const verseParts = versesString.split(",").filter(versePart => versePart.trim() !== "");
+
+                    verseParts.forEach((versePart) => {
+                        if (versePart.includes("-")) {
+                            let [start, end] = versePart.split("-").map(Number);
+                            if (!isNaN(start)) {
+                                end = end === 0 ? start : end;
+                                for (let verse = start; verse <= end; verse++) {
+                                    const key = `${surah}:${verse}`;
+                                    addVerseToCollection(key, surah, verse);
+                                }
+                            }
+                        } else {
+                            const verse = Number(versePart);
+                            if (!isNaN(verse)) {
+                                const key = `${surah}:${verse}`;
+                                addVerseToCollection(key, surah, verse);
+                            }
+                        }
+                    });
                 }
             });
-        });
-    
-        setVerseList(vl);
-        setTitleList(tl);
-    
-        function addVerseToCollection(key, surah, verse) {
-            if (quranmap && quranmap[surah]) {
-                vl[key] = quranmap[surah][verse];
-                if (quranmap[surah]["t" + verse]) {
-                    tl[key] = quranmap[surah]["t" + verse];
+
+            setVerseList(vl);
+            setTitleList(tl);
+
+            function addVerseToCollection(key, surah, verse) {
+                if (quranmap && quranmap[surah]) {
+                    vl[key] = quranmap[surah][verse];
+                    if (quranmap[surah]["t" + verse]) {
+                        tl[key] = quranmap[surah]["t" + verse];
+                    }
                 }
             }
+        } else {
+            setUf(true);
         }
-    
+
     }, [params, quranmap]);
-    
+
     return (
-        <div className={`select-text w-screen h-screen bg-gradient-to-r from-sky-400 to-cyan-400 pl-2 py-2 flex flex-col justify-center items-center`}>
-            <div className={`w-full lg:w-1/2 flex flex-col overflow-auto pr-2`}>
-                {Object.entries(verseList).map(([key, text]) => (
-                    <div key={key} className={` text-neutral-100 text-justify hyphens-auto p-1.5`}>
-                        {/* <div className={`text-center hyphens-auto p-1.5 text-neutral-900`}>
-                            {key}
-                        </div> */}
-                        {titleList[key] &&
-                            <div key={key + "title"} className={`rounded shadow-lg bg-sky-900/90 text-justify hyphens-auto py-2 px-2.5 mb-1.5`}>
-                                {titleList[key]}
-                            </div>}
-                        <div key={key + "verse"} className={`rounded shadow-lg bg-neutral-900/90 text-justify hyphens-auto py-1.5 px-2 mb-2`}>
-                            <span className={`text-sky-500`}>{key}</span> {text}
-                        </div>
-                    </div>
-                ))}
+        <div className="select-text fixed w-screen h-screen bg-gradient-to-r from-sky-500 to-cyan-500 pl-2 pb-2 flex flex-col justify-center items-center">
+            <div className="text-base h-12 w-12 md:h-20 md:w-20 lg:h-24 lg:w-24 absolute bottom-5 z-10">
+                <Link to="/">
+                    <img
+                        src="/logo512.png"
+                        alt="Logo"
+
+                        className="mt-4"
+                    />
+                </Link>
             </div>
+            {uf ? (
+                <div className="text-neutral-100 text-3xl md:text-6xl ">
+                    {loc.pathname + " ?"}
+                </div>
+            ) : (
+                <div className="w-full md:text-xl lg:w-3/4 lg:text-2xl flex flex-col overflow-auto pr-2 pb-12 md:pb-20 lg:pb-24 z-20">
+                    {Object.entries(verseList).map(([key, text]) => (
+                        <div key={key} className="text-neutral-100 text-justify hyphens-auto px-1.5 ">
+                            {titleList[key] && (
+                                <div
+                                    key={key + "title"}
+                                    className={`rounded-t whitespace-pre-line bg-gradient-to-r from-sky-500 via-blue-900/80 to-cyan-500 text-center py-2 px-2.5`}>
+                                    {titleList[key]}
+                                </div>
+                            )}
+                            <div
+                                key={key + "verse"}
+                                className={`${titleList[key] ? "rounded-b" : "rounded"} shadow-lg bg-neutral-900/95 text-justify hyphens-auto py-1.5 px-2 mb-2`}>
+                                <span className="text-sky-500">{key}</span> {text}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
