d91414ab6b8221250a221ec00864c8283b3b8ca5
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-08-13 17:19:46 +0300

    Enhance Leaf to support % as splitter in fomula
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/pages/Leaf.js
+++ app/src/pages/Leaf.js
@@ -37,46 +37,50 @@ const Leaf = () => {
         let vl = {};
         let tl = {};
         if (params && params.match(/^\d+:/)) {
-            params.split(";").forEach((param) => {
-                const trimmedParam = param.trim();
-                if (!trimmedParam) {
-                    return;
-                }
-
-                const [surah, versesString] = trimmedParam.split(":");
-                if (versesString === undefined || versesString.trim() === "") {
-                    if (quranmap && quranmap[surah]) {
-                        Object.keys(quranmap[surah]).forEach((verseKey) => {
-                            if (!verseKey.startsWith("t")) {
-                                const key = `${surah}:${verseKey}`;
-                                addVerseToCollection(key, surah, verseKey);
-                            }
-                        });
+            const formula = params.includes(";") ? params.split(";") : params.includes("%") ? params.split("%") : null;
+            if (formula) {
+                formula.forEach((param) => {
+                    const trimmedParam = param.trim();
+                    if (!trimmedParam) {
+                        return;
                     }
-                } else {
-                    const verseParts = versesString.split(",").filter(versePart => versePart.trim() !== "");
 
-                    verseParts.forEach((versePart) => {
-                        if (versePart.includes("-")) {
-                            let [start, end] = versePart.split("-").map(Number);
-                            if (!isNaN(start)) {
-                                end = end === 0 ? start : end;
-                                for (let verse = start; verse <= end; verse++) {
+                    const [surah, versesString] = trimmedParam.split(":");
+                    if (versesString === undefined || versesString.trim() === "") {
+                        if (quranmap && quranmap[surah]) {
+                            Object.keys(quranmap[surah]).forEach((verseKey) => {
+                                if (!verseKey.startsWith("t")) {
+                                    const key = `${surah}:${verseKey}`;
+                                    addVerseToCollection(key, surah, verseKey);
+                                }
+                            });
+                        }
+                    } else {
+                        const verseParts = versesString.split(",").filter(versePart => versePart.trim() !== "");
+
+                        verseParts.forEach((versePart) => {
+                            if (versePart.includes("-")) {
+                                let [start, end] = versePart.split("-").map(Number);
+                                if (!isNaN(start)) {
+                                    end = end === 0 ? start : end;
+                                    for (let verse = start; verse <= end; verse++) {
+                                        const key = `${surah}:${verse}`;
+                                        addVerseToCollection(key, surah, verse);
+                                    }
+                                }
+                            } else {
+                                const verse = Number(versePart);
+                                if (!isNaN(verse)) {
                                     const key = `${surah}:${verse}`;
                                     addVerseToCollection(key, surah, verse);
                                 }
                             }
-                        } else {
-                            const verse = Number(versePart);
-                            if (!isNaN(verse)) {
-                                const key = `${surah}:${verse}`;
-                                addVerseToCollection(key, surah, verse);
-                            }
-                        }
-                    });
-                }
-            });
-
+                        });
+                    }
+                });
+            } else {
+                setUf(true);
+            }
             setVerseList(vl);
             setTitleList(tl);
 
