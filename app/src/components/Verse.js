1d8a3e5484ca04a4e212f8b478dfd566d7a944ce
Yunusemre Şentürk <se.yunusemre@gmail.com>
2024-12-09 01:33:07 +0300

    Fix apperance 29:17 for RTL languages with a special hack
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/components/Verse.js
+++ app/src/components/Verse.js
@@ -497,19 +497,54 @@ const Verse = ({ besmele,
                                         return [{ line: element, combinedIndex: index }];
                                     })
                                         .map(({ line, indexToStart }, index) => {
-                                            const isTitle = parseInt(indexToStart) === 2;
-                                            return (
-                                                <React.Fragment key={index}>
-                                                    {index === 0 && typeof line === 'string' && (
-                                                        <span className={mode === "light" ? encryptedText.includes(besmele) ? `text-rose-500 font-semibold` : `${colors[theme]["matching-text"]} font-semibold` : `brightness-150`}>
-                                                            {`${verseNumber}. `}
+                                            // SPECIAL CASE FOR RTL 29:17 inner title HACK for proper demonstration
+                                            if (currentVerseKey === `29:17` && direction === 'rtl' && index !== 0) {
+                                                if (parseInt(index) === 5) {
+                                                    line = line?.props?.children + text[4].split('\n')[0];
+                                                    const isTitle = true;
+                                                    return (
+                                                        <React.Fragment key={index}>
+                                                            {index === 0 && typeof line === 'string' && (
+                                                                <span className={mode === "light" ? `${colors[theme]["matching-text"]} font-semibold` : `brightness-150`}>
+                                                                    {`${verseNumber}. `}
+                                                                </span>
+                                                            )}
+                                                            <span className={isTitle ? `text-center w-full block py-1.5 md:py-2 transform italic font-semibold ${colors[theme]["app-background"]} ${mode === `reading` ? `scale-x-[1.03]` : `scale-x-[1.033] `}` : ``}>
+                                                                {line}
+                                                            </span>
+                                                        </React.Fragment>
+                                                    );
+                                                } else if (parseInt(index) === 6) {
+                                                    return <></>;
+                                                } else {
+                                                    return (
+                                                        <React.Fragment key={index}>
+                                                            {index === 0 && typeof line === 'string' && (
+                                                                <span className={mode === "light" ? `${colors[theme]["matching-text"]} font-semibold` : `brightness-150`}>
+                                                                    {`${verseNumber}. `}
+                                                                </span>
+                                                            )}
+                                                            <span>
+                                                                {line}
+                                                            </span>
+                                                        </React.Fragment>
+                                                    );
+                                                }
+                                            } else {
+                                                const isTitle = parseInt(indexToStart) === 2;
+                                                return (
+                                                    <React.Fragment key={index}>
+                                                        {index === 0 && typeof line === 'string' && (
+                                                            <span className={mode === "light" ? `${colors[theme]["matching-text"]} font-semibold` : `brightness-150`}>
+                                                                {`${verseNumber}. `}
+                                                            </span>
+                                                        )}
+                                                        <span className={isTitle ? `text-center w-full block py-1.5 md:py-2 transform italic font-semibold ${colors[theme]["app-background"]} ${mode === `reading` ? `scale-x-[1.03]` : `scale-x-[1.033] `}` : ``}>
+                                                            {line}
                                                         </span>
-                                                    )}
-                                                    <span className={isTitle ? `text-center w-full ${colors[theme]["app-background"]} block py-1.5 md:py-2 transform italic font-semibold ${isTitle ? 'scale-x-[1.035]' : ''}` : ``}>
-                                                        {line}
-                                                    </span>
-                                                </React.Fragment>
-                                            );
+                                                    </React.Fragment>
+                                                );
+                                            }
                                         })
                                 ) : (
                                     text.map((element, index) => (
