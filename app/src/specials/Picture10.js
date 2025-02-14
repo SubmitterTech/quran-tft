cefe91633488538291c8a1564e40b6ef13963766
Yunusemre Şentürk <se.yunusemre@gmail.com>
2025-02-14 14:07:40 +0300

    Fix picture10's overflow direction
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/specials/Picture10.js
+++ app/src/specials/Picture10.js
@@ -6,7 +6,7 @@ const Picture10 = ({ item, imageUrl, colors, theme }) => {
     const textColor = colors[theme]["log-text"];
     return (
         <div key={`picture-10-special`} className={`flex flex-col flex-1 items-center justify-center w-full px-1`}>
-            <div className={`flex p-1 overflow-y-auto`}>
+            <div className={`flex px-1 pt-1 pb-3 overflow-x-auto`}>
                 <div className={` flex flex-col justify-between `}>
                     {data.slice(0, 4).map((word) => (
                         <div key={word} className={`p-1.5 whitespace-nowrap text-right`}>{word}</div>
