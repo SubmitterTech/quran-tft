487add728d360f678c53b5cfb0b04f181e20ed7b
Yunusemre Şentürk <se.yunusemre@gmail.com>
2026-03-03 12:51:54 +0300

    make popup of deletion of a bookmark respect new-lines
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/src/components/Pages.js
+++ app/src/components/Pages.js
@@ -1071,7 +1071,7 @@ const Pages = React.memo(({
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
                                         </svg>
                                     </div>
-                                    <div className={`w-full text-lg rounded p-1 text-start ${colors[theme]["matching-text"]} ${colors[theme]["encrypted-background"]}`}>
+                                    <div className={`w-full text-lg rounded p-1 text-start whitespace-pre-line ${colors[theme]["matching-text"]} ${colors[theme]["encrypted-background"]}`}>
                                         {`${deleteConfirmResolver.data.value}`}
                                     </div>
                                 </div>
